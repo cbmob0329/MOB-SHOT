3f6d3117857a1f943d5cae4cf8af6596d213c532
cbmob0329 <pb2matsuyamadance@gmail.com>
2026-06-19 11:17:37 +0900

    Update preload.js

--- js/preload.js
+++ js/preload.js
@@ -1,12 +1,10 @@
 'use strict';
 
 (function(){
-  const ASSETS = [
+  const PRELOAD_ASSETS = [
+    // main
     'mt/mainback.png',
     'mt/menutitle.png',
-    'play/playpink2.png',
-    'play/playpink.png',
-
     'mt/menusta.png',
     'mt/menuevent.png',
     'mt/menushop.png',
@@ -16,19 +14,93 @@
     'mt/menumission.png',
     'mt/menucolle.png',
 
+    // player default
+    'play/playpink2.png',
+    'play/playpink.png',
+
+    // hud
+    'mt/stagestage.png',
+    'mt/stagescore.png',
+    'mt/stagecoin.png',
+    'mt/stagelife.png',
+
+    // stage backgrounds
     'sta/backsougen.png',
     'sta/backsabaku.png',
     'sta/backumi.png',
     'sta/backneon.png',
     'sta/backmagma.png',
     'sta/backmao.png',
+    'sta/stkan.png',
+    'sta/stmatrix.png',
+    'sta/umisenro.png',
+    'sta/neonlord.png',
+    'sta/makai.png',
+    'sta/makailast.png',
 
+    // bullets
+    'mt/atk.png',
+    'atk/hinotama.png',
+
+    // chests
     'gimi/takagin.png',
     'gimi/takagol.png',
+
+    // gimmicks
     'gimi/gimihako.png',
+    'gimi/gimikan.png',
     'gimi/gimiiwa.png',
-    'atk/hinotama.png',
-    'mt/atk.png'
+    'gimi/gimitaru.png',
+    'gimi/gimisabakiwa.png',
+    'gimi/gimidou.png',
+    'gimi/gimichari.png',
+    'gimi/gimitaiya.png',
+    'gimi/gimihan.png',
+    'gimi/gimineonspi.png',
+    'gimi/gimineonreco.png',
+    'gimi/neonhunsui.png',
+    'gimi/gimimag.png',
+    'gimi/gimimaghasi.png',
+    'gimi/gimimagspi.png',
+    'gimi/gimiseki.png',
+    'gimi/gimimaotama.png',
+    'gimi/gimimao.png',
+
+    // enemies
+    'en/sra.png',
+    'en/eniwa.png',
+    'en/entozok.png',
+    'en/endowa.png',
+    'en/enwasi.png',
+    'en/iwakofal.png',
+    'en/ennarga.png',
+    'en/enguri.png',
+    'en/enmagtokage.png',
+    'en/enmagpte.png',
+    'en/enmaogob.png',
+    'en/enasa.png',
+
+    // mid bosses
+    'en/enpte.png',
+    'en/sabadual.png',
+    'en/enmobpi.png',
+    'en/neongidra.png',
+    'en/enmaggolem.png',
+    'en/mobgra.png',
+
+    // bosses
+    'boss/hawks.png',
+    'boss/hawks2.png',
+    'boss/miraboss.png',
+    'boss/bossmira2.png',
+    'boss/bossban.png',
+    'boss/bossban2.png',
+    'boss/bossneon.png',
+    'boss/bossneon2.png',
+    'boss/bossdragoon.png',
+    'boss/bossdragoon2.png',
+    'boss/bossriris.png',
+    'boss/bossmaoh.png'
   ];
 
   function $(id){
@@ -39,7 +111,7 @@
     return Array.from(new Set(list.filter(Boolean)));
   }
 
-  function setProgress(done,total){
+  function setProgress(done, total){
     const percent = total <= 0 ? 100 : Math.floor((done / total) * 100);
     const text = $('loadingPercent');
     const bar = $('loadingBarFill');
@@ -54,13 +126,14 @@
 
       image.onload = function(){ resolve(true); };
       image.onerror = function(){ resolve(false); };
+
       image.src = src;
     });
   }
 
   async function start(){
     const loading = $('loadingScreen');
-    const assets = unique(ASSETS);
+    const assets = unique(PRELOAD_ASSETS);
     let done = 0;
 
     setProgress(0, assets.length);
